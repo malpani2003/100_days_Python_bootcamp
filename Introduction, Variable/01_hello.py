d331df8a9a01488bde838f0113d602a3d4b03707
malpani2003 <98647622+malpani2003@users.noreply.github.com>
2022-09-21 21:04:44 +0530

    Add files via upload

---
+++ Introduction, Variable/01_hello.py
@@ -0,0 +1,16 @@
+import os  # importing os module 
+
+print("hello world!") #just use to  print in console
+
+print("My name is \n pranav Malpani")
+
+# \n is also used here to craete a new line
+
+#Triple single quote are also used in print function to prinction long strings like by line
+
+print('''Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')''') 
+
+ #+ is used a string concetation
+print("Learning + Python")
---
+++ Introduction, Variable/02_variable_data.py
@@ -0,0 +1,36 @@
+
+#Data_type
+
+# 1.String
+# 2.int
+# 3.boolean
+# 4.float
+# 5.complex
+
+a=3012345678901234567890  #int
+print(a)
+
+d="Pranav"  #str 
+print(d)
+print(d[2])
+print(d[-1]) # negative index is used to get char from end means -1 give last char 
+
+b=25.45    #float
+print(b)
+c=3+4j    #complex
+print(c)
+e=True   #bool
+# In true / false write first alphabate always capiatl
+print(e)
+f=None   #none type
+print(f)
+
+#type() is detect data type of a variable
+#printing type of variable
+print(type(a))
+
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
